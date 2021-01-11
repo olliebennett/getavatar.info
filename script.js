5fc48c9d61bae315ed5f7204c7b87ff16f86315b
Ollie Bennett <olliebennett@gmail.com>
2021-01-11 20:29:40 +0000

    Improve user data display and show results table

--- script.js
+++ script.js
@@ -6,50 +6,69 @@ var handleGravatarResponse = function(profile) {
   }
   document.getElementById('displayName').innerHTML = user.displayName;
   document.getElementById('preferredUsername').innerHTML = user.preferredUsername;
-  document.getElementById('userData').innerHTML = JSON.stringify(user);
+  document.getElementById('gravitarImage').src = user.thumbnailUrl;
   guessEmailAddress(user.hash, user.preferredUsername, user.name.givenName, user.name.familyName);
 };
 
 var guessEmailAddress = function(hash, username, firstname, lastname) {
-  var basicMailboxes = [];
+  var mailboxes = [];
   if (username) {
     username = username.toLowerCase();
-    basicMailboxes.push(username);
+    mailboxes.push(username);
   }
   if (firstname) {
     firstname = firstname.toLowerCase();
-    basicMailboxes.push(firstname);
+    mailboxes.push(firstname);
   }
   if (lastname) {
     lastname = lastname.toLowerCase();
-    basicMailboxes.push(lastname);
+    mailboxes.push(lastname);
   }
 
-  // Assume people use their birthdays!
-  var numberList = [''].concat(range(1980, 2005));
+  mailboxes.concat(nameCombinations(firstname, lastname));
 
-  var seps = ['', '.', '-', '_'];
-  if (firstname && lastname) {
-    seps.forEach(function(sep1) {
-      basicMailboxes.push(firstname + sep1 + lastname)
-      basicMailboxes.push(lastname + sep1 + firstname)
-      basicMailboxes.push(firstname.substring(0, 1) + sep1 + lastname)
-      basicMailboxes.push(firstname + sep1 + lastname.substring(0, 1))
-    });
-  }
-
-  var possibleMailboxes = [];
-  numberList.forEach(function(num) {
-    basicMailboxes.forEach(function(basicMailbox) {
-      possibleMailboxes.push(basicMailbox + num);
-    });
-  });
+  mailboxes.concat(yearCombinations(mailboxes))
 
   // De-duplicate...
-  possibleMailboxes = possibleMailboxes.filter(function(item, pos, self) { return self.indexOf(item) == pos; });
+  mailboxes = uniq(mailboxes);
 
-  return checkEmailAddresses(hash, possibleMailboxes, primaryDomains);
+  return checkEmailAddresses(hash, mailboxes, primaryDomains);
 };
+
+var nameCombinations = function(firstname, lastname) {
+  if (!firstname || !lastname) { return []; };
+
+  var result = [];
+  ['', '.', '-', '_'].forEach(function(sep) {
+    result.push(firstname + sep + lastname)
+    result.push(lastname + sep + firstname)
+    result.push(firstname.substring(0, 1) + sep + lastname)
+    result.push(firstname + sep + lastname.substring(0, 1))
+  });
+
+  return result;
+};
+
+var years = function() {
+  // It's common to use birth-year, or year of email registration (i.e. up to current year)
+  // ... and either in full or the last 2 digits
+  var yearList = range(1980, (new Date).getFullYear()).map(function(y) { return y.toString() });
+  var yearSuffixList = yearList.map(function(y) { return y.substring(2,4) });
+  return yearList.concat(yearSuffixList);
+}
+
+var yearCombinations = function(nameList) {
+  var result = [];
+  years().forEach(function(year) {
+    nameList.forEach(function(name) {
+      result.push(name + year);
+    });
+  });
+}
+
+// var containsYear = function(str) {
+//   /[0-9]{2,4}/g.test(str);
+// }
 
 var range = function(min, max) {
   var x = [];
@@ -57,6 +76,10 @@ var range = function(min, max) {
     x.push(i);
   }
   return x;
+}
+
+var uniq = function(array) {
+  return array.filter(function(item, pos, self) { return self.indexOf(item) == pos; });
 }
 
 var toggleLoading = function(bool, delay) {
@@ -69,13 +92,15 @@ var toggleLoading = function(bool, delay) {
 
 var checkEmailAddresses = function(hash, mailboxes, domains) {
   toggleLoading(true, false);
+  checksTableReset();
   // Try every combination of mailbox@domain
   var mailboxes_count = mailboxes.length;
   var domains_count = domains.length;
-  for (var m = 0; m < mailboxes_count; m++) {
-    var mailbox = mailboxes[m];
-    for (var d = 0; d < domains_count; d++) {
-      var domain = domains[d];
+  for (var d = 0; d < domains_count; d++) {
+    var domain = domains[d];
+    checksTableNewDomainRow(domain);
+    for (var m = 0; m < mailboxes_count; m++) {
+      var mailbox = mailboxes[m];
       var test_email = mailbox + '@' + domain;
       var test_hash = MD5(test_email);
       if ((m * d) % 1000 === 0) {
@@ -83,10 +108,17 @@ var checkEmailAddresses = function(hash, mailboxes, domains) {
       }
       if (test_hash == hash) {
         foundMatch(test_email);
+        toggleLoading(false, false);
+        checksTableUpdateResult(domain, 'check1', true);
+        checksTableUpdateResult(domain, 'check2', true);
         return test_email;
       }
     };
+    checksTableUpdateResult(domain, 'check1', false);
+    checksTableUpdateResult(domain, 'check2', false);
   };
+
+  toggleLoading(false, true);
 
   return null; // no match :(
 }
@@ -111,6 +143,35 @@ var getBasicInfo = function(hash) {
   document.head.appendChild(script);
 };
 
+var checksTableReset = function() {
+  document.getElementById('userData').style.display = 'block';
+  var checksTableBody = document.getElementById('checksTable').getElementsByTagName('tbody')[0];
+  checksTableBody.innerHTML = '';
+};
+
+var checksTableNewDomainRow = function(domain) {
+  var checksTableBody = document.getElementById('checksTable').getElementsByTagName('tbody')[0];
+
+  var td0 = document.createElement("td");
+  td0.innerHTML = domain;
+  var td1 = document.createElement("td");
+  td1.innerHTML = '...';
+  td1.id = domain.replace('.', '_') + '_check1';
+  var td2 = document.createElement("td");
+  td2.innerHTML = '...';
+  td2.id = domain.replace('.', '_') + '_check2';
+  var tr = document.createElement("tr");
+  tr.appendChild(td0);
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  checksTableBody.appendChild(tr);
+};
+
+var checksTableUpdateResult = function(domain, check_slug, success_bool) {
+  var el = document.getElementById(domain.replace('.', '_') + '_' + check_slug);
+  el.innerHTML = success_bool ? 'FOUND' : 'no match';
+};
+
 window.addEventListener("load", function load(event){
   window.removeEventListener("load", load, false); //remove listener, no longer needed
 
@@ -132,5 +193,6 @@ window.addEventListener("load", function load(event){
     });
   }
 
+  document.getElementById('userData').style.display = 'none';
   toggleLoading(false, false);
 }, false);
